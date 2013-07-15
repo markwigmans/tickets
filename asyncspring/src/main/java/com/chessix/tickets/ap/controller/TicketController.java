3a20e9a50410c4c9247f32555f6756522c0cf61b
Mark Wigmans <mawi@PC-iTi-189.CHESS.INT>
2013-07-15 20:14:13 +0200

    initial version

---
+++ asyncspring/src/main/java/com/chessix/tickets/ap/controller/TicketController.java
@@ -0,0 +1,87 @@
+package com.chessix.tickets.ap.controller;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.chessix.tickets.ap.model.BoxOffice;
+import com.chessix.tickets.messages.AbstractTicketMessage;
+import com.chessix.tickets.messages.EventCreated;
+import com.chessix.tickets.utils.EventUtils;
+
+/**
+ * 
+ * @author Mark Wigmans
+ * 
+ */
+@Controller
+public class TicketController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * Remember which event manager to use for a given event.
+     */
+    private final ConcurrentMap<String, BoxOffice> eventManagers = new ConcurrentHashMap<>();
+
+    /**
+     * Create given event
+     * 
+     * @param event
+     *            name of event
+     * @param tickets
+     *            number of available tickets
+     */
+    @RequestMapping(value = "/event/{event}/{tickets}", method = RequestMethod.POST)
+    @ResponseBody
+    public EventCreated createEvent(@PathVariable final String event, @PathVariable final int tickets) {
+        logger.info("createEvent() : {}", event);
+        final String key = EventUtils.getEventName(event);
+
+        final EventCreated result;
+        if (!eventManagers.containsKey(key)) {
+            final BoxOffice manager = new BoxOffice(event, tickets);
+            logger.info("manager created: {}", manager);
+            eventManagers.putIfAbsent(key, manager);
+            result = new EventCreated(key);
+        } else {
+            result = new EventCreated(key, false, "Already created");
+        }
+        return result;
+    }
+
+    /**
+     * Request 1 ticket to buy
+     */
+    @RequestMapping(value = "/ticket/{event}", method = RequestMethod.POST)
+    @ResponseBody
+    public Callable<AbstractTicketMessage> buySingle(@PathVariable final String event) {
+        return buyMultiple(event, 1);
+    }
+
+    /**
+     * Request {@code tickets} ticket(s) to buy
+     */
+    @RequestMapping(value = "/ticket/{event}/{tickets}", method = RequestMethod.POST)
+    @ResponseBody
+    public Callable<AbstractTicketMessage> buyMultiple(@PathVariable final String event, @PathVariable final int tickets) {
+        return new Callable<AbstractTicketMessage>() {
+            public AbstractTicketMessage call() throws Exception {
+                logger.debug("buy({},{})", event, tickets);
+                final BoxOffice manager = eventManagers.get(EventUtils.getEventName(event));
+                if (manager != null) {
+                    return manager.buy(tickets);
+                }
+                throw new IllegalArgumentException(String.format("Unknown event: '%s'", event));
+            }
+        };
+    }
+}
---
+++ asyncspring/src/main/java/com/chessix/tickets/ap/model/BoxOffice.java
@@ -0,0 +1,53 @@
+package com.chessix.tickets.ap.model;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.chessix.tickets.messages.AbstractTicketMessage;
+import com.chessix.tickets.messages.ReturnTickets;
+import com.chessix.tickets.messages.SoldOut;
+import com.chessix.tickets.utils.EventUtils;
+
+/**
+ * 
+ * 
+ * @author Mark Wigmans
+ * 
+ */
+public class BoxOffice {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final String event;
+    private final AtomicInteger availableTickets;
+
+    /**
+	 * 
+	 */
+    public BoxOffice(final String event, final int availableTickets) {
+        super();
+        this.event = event;
+        this.availableTickets = new AtomicInteger(availableTickets);
+    }
+
+    /**
+     * Buy the given number of tickets.
+     * 
+     * @throws InterruptedException
+     */
+    public AbstractTicketMessage buy(final int tickets) {
+        logger.debug("event: '{}' : buy({})", event, tickets);
+        EventUtils.busy();
+        final int newValue = availableTickets.addAndGet(-tickets);
+        if (newValue < 0) {
+            logger.debug("event: '{}' : sold out", event);
+            availableTickets.addAndGet(tickets);
+            return new SoldOut(event);
+        } else {
+            return new ReturnTickets(event, tickets);
+        }
+    }
+
+}
